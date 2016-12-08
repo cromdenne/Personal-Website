479daab6bc78e57028e2a1867391a7e6bc237ec0
Cory Romdenne <cory@romdenne.com>
2016-12-08 14:23:38 -0600

    Sectioning & about
    Set up each content section. Added content for about section.

--- index.php
+++ index.php
@@ -23,8 +23,43 @@
     </head>
     
     <body>
-    
-        <h1>Hello, world!</h1>
+        
+        <div class="container">
+            
+            <div class="row bg-light" id="banner">
+                <div class="text-center">
+                    <a href="index.php"><img src="images/romdenne-logo.png" alt="Cory Romdenne's personal logo" width="25%"></a>
+                    <h1>CORY ROMDENNE</h1>
+                </div><!-- /.text-center -->
+            </div><!-- /#banner -->
+            <div class="row bg-dark" id="about">
+                <div class="col-md-2 col-md-offset-1 text-center">
+                    <img src="http://fakeimg.pl/250x250/?text=Hello!" alt="Photo of Cory Romdenne" class="img-circle" width="100%">
+                </div><!-- /.col-md-2 col-md-offset-1 text-center -->
+                <div class="col-md-8">
+                    <h3>Hi there! My name is Cory. I’m passionate about soccer, technology, photography, board games and books, among other things. I’m 27 years old. Green Bay, Wisconsin is my home.</h3>
+                    <br>
+                    <p>I enjoy helping people solve problems. I also like to build things. I’ve found web development to be a rewarding intersection of the two and it’s been an interest of mine ever since my first job maintaining websites for the UW - Madison College of Letters and Sciences in 2011. I enjoy developing elegant front-end interfaces that users love. I write in HTML, CSS, Javascript and PHP. I design with Adobe Photoshop and Illustrator. I love to learn.</p>
+                </div><!-- /.col-md-8 -->
+            </div><!-- /#about -->
+            <div class="row bg-light" id="skills">
+                <div class="text-center">
+                    <h2>SKILLS</h2>
+                </div><!-- /.text-center -->
+            </div><!-- /#skills -->
+            <div class="row bg-dark" id="work">
+                <div class="text-center">
+                    <h2>WORK</h2>
+                </div><!-- /.text-center -->
+            </div><!-- /#work -->
+            <div class="row bg-light" id="contact">
+                <div class="text-center">
+                    <h2>CONTACT</h2>
+                </div><!-- /.text-center -->
+            </div><!-- /#contact -->
+            
+        </div><!-- /.container -->
+        
     
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
