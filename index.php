1d22aba985e11430d1b741e162fc0d259da72eac
Cory Romdenne <cory@romdenne.com>
2016-12-08 13:13:31 -0600

    Embark
    Set up to create a personal website. Folder structure created.
    Appropriate Bootstrap css and js files added and linked. Basic index
    page created. Images folder with logo added. Fonts folder with Bootstrap
    icon fonts added.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+    
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Cory Romdenne - Web developer and soccer junkie. Green Bay, Wisconsin.">
+        <meta name="keywords" content="HTML,CSS,JavaScript,web developer,Green Bay,Wisconsin">
+        <meta name="author" content="Cory Romdenne">
+        
+        <title>Cory Romdenne</title>
+    
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- custom styles -->
+        <link href="css/style.css" rel="stylesheet">
+        
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Muli:300|Poiret+One" rel="stylesheet">
+        
+    </head>
+    
+    <body>
+    
+        <h1>Hello, world!</h1>
+    
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+        <!-- Include all compiled plugins -->
+        <script src="js/bootstrap.min.js"></script>
+        
+    </body>
+
+</html>
