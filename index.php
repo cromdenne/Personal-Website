37b025be19060d56eeb0de5f252a9690a9d21334
Cory Romdenne <cory@romdenne.com>
2016-12-08 14:58:15 -0600

    Margins, padding, fonts
    Updated the layout of the page for a cleaner, blocked look. Updated
    classes for sections and backgrounds. Updated header and body fonts.

--- index.php
+++ index.php
@@ -18,41 +18,42 @@
         <link href="css/style.css" rel="stylesheet">
         
         <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Muli:300|Poiret+One" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Krona+One|Reem+Kufi" rel="stylesheet">
         
     </head>
     
     <body>
         
-        <div class="container">
+        <div class="container-fluid">
             
-            <div class="row bg-light" id="banner">
+            <div class="row brick" id="banner">
                 <div class="text-center">
                     <a href="index.php"><img src="images/romdenne-logo.png" alt="Cory Romdenne's personal logo" width="25%"></a>
-                    <h1>CORY ROMDENNE</h1>
+                    <h1 class="title">CORY ROMDENNE</h1>
                 </div><!-- /.text-center -->
             </div><!-- /#banner -->
-            <div class="row bg-dark" id="about">
-                <div class="col-md-2 col-md-offset-1 text-center">
-                    <img src="http://fakeimg.pl/250x250/?text=Hello!" alt="Photo of Cory Romdenne" class="img-circle" width="100%">
+            <div class="row brick dark" id="about">
+                <div class="col-md-3">
+                    <!-- check out janlosert.com for text over image effect in small viewport -->
+                    <img src="http://fakeimg.pl/250x250/?text=Hello!" alt="Photo of Cory Romdenne" class="img-circle" width="75%">
                 </div><!-- /.col-md-2 col-md-offset-1 text-center -->
-                <div class="col-md-8">
-                    <h3>Hi there! My name is Cory. I’m passionate about soccer, technology, photography, board games and books, among other things. I’m 27 years old. Green Bay, Wisconsin is my home.</h3>
-                    <br>
-                    <p>I enjoy helping people solve problems. I also like to build things. I’ve found web development to be a rewarding intersection of the two and it’s been an interest of mine ever since my first job maintaining websites for the UW - Madison College of Letters and Sciences in 2011. I enjoy developing elegant front-end interfaces that users love. I write in HTML, CSS, Javascript and PHP. I design with Adobe Photoshop and Illustrator. I love to learn.</p>
+                <div class="col-md-9">
+                    <p class="emphasize">Hi there! My name is Cory.</p>
+                    <p>I’m passionate about soccer, technology, photography, board games and books, among other things. I’m 27 years old. Green Bay, Wisconsin is my home.</p>
+                    <p>I enjoy helping people solve problems. I also like to build things. I’ve found web development to be a rewarding intersection of the two and it’s been an interest of mine ever since my first job maintaining websites for the UW - Madison College of Letters and Sciences. I enjoy developing elegant front-end interfaces that users love. I write in HTML, CSS, Javascript and PHP. I design with Adobe Photoshop and Illustrator. I love to learn.</p>
                 </div><!-- /.col-md-8 -->
             </div><!-- /#about -->
-            <div class="row bg-light" id="skills">
+            <div class="row brick" id="skills">
                 <div class="text-center">
                     <h2>SKILLS</h2>
                 </div><!-- /.text-center -->
             </div><!-- /#skills -->
-            <div class="row bg-dark" id="work">
+            <div class="row brick dark" id="work">
                 <div class="text-center">
                     <h2>WORK</h2>
                 </div><!-- /.text-center -->
             </div><!-- /#work -->
-            <div class="row bg-light" id="contact">
+            <div class="row brick" id="contact">
                 <div class="text-center">
                     <h2>CONTACT</h2>
                 </div><!-- /.text-center -->
